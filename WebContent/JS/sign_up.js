a116e38f18deaa3bd6d2b211fb55188fbe419eae
koys0818 <koys0818@naver.com>
2020-06-01 14:30:02 +0900

    06-01 게시판 2차 수정

--- WebContent/JS/sign_up.js
+++ WebContent/JS/sign_up.js
@@ -2,3 +2,12 @@
 function goBack() {
   window.history.back();
 }
+
+$(document).ready(function(){
+
+    $("#email_site").change(function(){
+        $("#email_adress").val($("#email_site option:selected").val());
+    });
+    
+
+});
