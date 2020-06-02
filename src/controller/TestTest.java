c5b833c9953aa72c7efd723b8b95de79c95babf5
writerkang <goodwriterkang@gmail.com>
2020-06-02 13:58:36 +0900

    sadsasa

--- src/controller/TestTest.java
+++ src/controller/TestTest.java
@@ -38,6 +38,8 @@ public class TestTest extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		//test
 	}
 
 }
