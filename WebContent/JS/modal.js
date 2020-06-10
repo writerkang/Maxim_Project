cfc1fe88644fc02fc2ebd4e6c54419bd0881a5ff
yusung <koys0818@naver.com>
2020-06-10 16:19:26 +0900

    modal

---
+++ WebContent/JS/modal.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+  })
+  
+  $('#modal-container').click(function(){
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+  });
+
+});
